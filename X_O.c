8fc8240dec190a695c5fdc4d4b665289c7d18dd2
abdel-ke <kemmal1337@gmail.com>
2021-05-24 22:23:56 +0100

    add_color

--- X_O.c
+++ X_O.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+# define GREEN	"\e[1;32m"
+# define RED	"\e[0;31m"
+# define WHITE	"\033[0m"
+# define BLUE	"\e[0;34m"
+# define YEL	"\e[0;33m"
+# define MAG	"\e[0;35m"
+# define CYN	"\e[0;36m"
 
 char	ch[10] = {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+char	*nbr_with_clr(char c)
+{
+	char *str;
+	str = strdup("\033[0mX\e[0;33m");
+	str[4] = c;
+	return str;
+}
+
+char	*check_char(char c)
+{
+	if (c == 'X')
+		return "\e[0;35mX\e[0;33m";
+	else if (c == 'O')
+		return "\e[0;36mO\e[0;33m";
+	else
+		return nbr_with_clr(c);
+	return NULL;
+}
 
 void	affichage()
 {
 	system("clear");
 	int i = 1;
-	puts(" _______________________________________________");
+	printf("%s _______________________________________________\n", YEL);
 	while (i < 10)
 	{
 		if (i + 2 <= 10)
 		{
 			printf("|		|		|		|\n");
-			printf("|	%c	|	%c	|	%c	|\n", ch[i] , ch[i + 1], ch[i + 2]);
-			  puts("|_______________|_______________|_______________|");
+			printf("|	%s	|	%s	|	%s	|\n", check_char(ch[i]) , check_char(ch[i + 1]), check_char(ch[i + 2]));
+			printf("|_______________|_______________|_______________|\n");
 		}
 		i += 3;
 	}
+	printf("%s", WHITE);
 }
 
 int		ret_checker(char type, int ret)
@@ -47,7 +75,7 @@ int		checker()
 		ret = 0;
 	if (ret_checker('X', 3))
 		return 3;
-	if (ret_checker('X', 3))
+	if (ret_checker('O', 3))
 		return 4;
 	i = 0;
 	while (ch[++i])
@@ -60,10 +88,13 @@ int		get_value(char type)
 {
 	int player;
 	do{
-		printf("\nPLAYER [%c]: PRINT A NUMBER : ", type);
+		if (type == 'X')
+			printf("%s\nPLAYER[%c]: PRINT A NUMBER : %s", MAG, type, WHITE);
+		else if (type == 'O')
+			printf("%s\nPLAYER[%c]: PRINT A NUMBER : %s", CYN, type, WHITE);
 		scanf("%d", &player);
 		if (player <= 0 || player > 9)
-			printf("\nERROR NUMBER");
+			printf("%s\nERROR NUMBER%s", RED, WHITE);
 	} while (player <= 0 || player > 9);
 	return player;
 }
@@ -77,7 +108,7 @@ void	player(int *cp, char type)
 		do
 		{
 			if (ch[player] == 'O' || ch[player] == 'X')
-				printf("\nPLACE INCORRECT");
+				printf("%s\nPLACE INCORRECT%s", RED, WHITE);
 			player = get_value(type);
 		} while (ch[player] == 'O' || ch[player] == 'X');
 	}
@@ -91,7 +122,7 @@ void	player(int *cp, char type)
 
 int		main()
 {
-	int cp = 1, ret = 0;
+	int cp = 1,  ret = 0;
 	affichage();
 	while ((ret = checker()) == 0)
 	{
@@ -101,9 +132,9 @@ int		main()
 			player(&cp, 'O');
 	}
 	if (ret == 3)
-		printf("PLAYER [X]: WON :D :D :D\n");
+		printf("%sPLAYER[%sX%s]: WON :D :D :D%s\n", GREEN, MAG, GREEN, WHITE);
 	else if (ret == 4)
-		printf("PLAYER [O]: WON :D :D :D\n");
+		printf("%sPLAYER[%sO%s]: WON :D :D :D\n%s", GREEN, CYN, GREEN, WHITE);
 	else if (ret == 2)
-		printf("PARTIE TERMINE :( :(\n");
+		printf("%sPARTIE TERMINE NO WINNER :( :(\n%s", BLUE, WHITE);
 }
